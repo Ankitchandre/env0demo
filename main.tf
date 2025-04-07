d0a6e6441bdf6251cc12b6c55d3d9fffecc21cc9
ankitchandre <krutikanatu@Krutikas-MacBook-Air.local>
2025-04-07 17:21:48 +0530

    add readme

--- main.tf
+++ main.tf
@@ -137,7 +137,7 @@ data "aws_ami" "amazon_linux" {
 # EC2 Instance
 resource "aws_instance" "app_server" {
   ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = var.instance_type
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public[0].id
   vpc_security_group_ids = [aws_security_group.ec2.id]
   key_name               = var.key_name != "" ? var.key_name : null
